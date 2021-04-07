07b80718dc1c8e46f761ccc8dd7b5ef13289f9be
Eric Menendez <3814114+emenendez@users.noreply.github.com>
2021-04-07 12:54:42 -0600

    Working sync

--- sync.py
+++ sync.py
@@ -1,17 +1,32 @@
-import asyncio
-from contextlib import asynccontextmanager
+from contextlib import contextmanager
 from dataclasses import dataclass, fields
 from furl import furl
 import logging
-from pathlib import Path
+import os
 import re
+import tempfile
 import typing as t
+from functools import partial
+from multiprocessing.pool import ThreadPool
+from pathlib import Path
 
-import aiohttp
-import asyncclick as click
+import click
+import ffmpeg
 import keyring
+import requests
+from mutagen.easyid3 import EasyID3
+from rich.logging import RichHandler
 
 from ipod import Shuffler
+
+
+logging.basicConfig(
+    level="INFO",
+    format="%(message)s",
+    datefmt="[%X]",
+    handlers=[RichHandler(rich_tracebacks=True)]
+)
+log = logging.getLogger(__name__)
 
 
 @dataclass(frozen=True)
@@ -19,6 +34,8 @@ class Episode:
     order: int
     uuid: str
     url: furl
+    title: str
+    podcast: str
 
     @classmethod
     def from_dict(cls, order: int, dict: t.Dict) -> "Episode":
@@ -33,95 +50,131 @@ class Episode:
 
     @property
     def filename(self) -> str:
-        return f"{self.order:02d}_{self.uuid}{self.extension}"
+        return f"{self.uuid}{self.extension}"
 
 
 @dataclass
 class PocketCasts:
-    session: aiohttp.ClientSession
+    session: requests.Session
 
-    async def json(self, method: str, *args, **kwargs) -> t.Dict:
+    def json(self, method: str, *args, **kwargs) -> t.Dict:
         method_call = getattr(self.session, method)
-        async with method_call(*args, **kwargs) as response:
-            return await response.json()
+        with method_call(*args, **kwargs) as response:
+            return response.json()
 
-    async def up_next(self) -> t.List[Episode]:
-        response = await self.json("post", "https://api.pocketcasts.com/up_next/list", json={"version": 2})
+    def up_next(self) -> t.List[Episode]:
+        response = self.json("post", "https://api.pocketcasts.com/up_next/list", json={"version": 2})
         return [Episode.from_dict(*item) for item in enumerate(response["episodes"])]
 
+    def podcasts(self) -> t.Dict[str, str]:
+        response = self.json("post", "https://api.pocketcasts.com/user/podcast/list", json={"v": 1})
+        return {podcast['uuid']: podcast['title'] for podcast in response['podcasts']}
 
-@asynccontextmanager
-async def pocket_casts():
+
+@contextmanager
+def pocket_casts():
     username: str = keyring.get_credential("Pocket Casts", "username").password
     password: str = keyring.get_credential("Pocket Casts", username).password
-    async with aiohttp.ClientSession() as session:
-        async with session.post('https://api.pocketcasts.com/user/login',
-                                json={"email": username, "password": password}) as response:
-            content = await response.json()
+    with requests.Session() as session:
+        with session.post('https://api.pocketcasts.com/user/login',
+                          json={"email": username, "password": password}) as response:
+            content = response.json()
             token = content["token"]
         session.headers.update({"Authorization": f"Bearer {token}"})
         yield PocketCasts(session)
 
 
-async def get_url(session: aiohttp.ClientSession, url: furl) -> bytes:
-    async with session.get(str(url)) as response:
-        return await response.read()
-
-
-async def download_episode(session: aiohttp.ClientSession, playlist_path: Path, episode: Episode) -> t.Tuple[bool, Episode]:
+def speedup(input: str, output: str) -> None:
+    options = {}
     try:
-        contents = await get_url(session, episode.url)
-        with (playlist_path / episode.filename).open("wb") as output:
-            output.write(contents)
+        options['audio_bitrate'] = ffmpeg.probe(input)['format']['bit_rate']
     except Exception:
-        logging.exception("Error downloading %s", episode.url)
+        pass
+
+    (
+        ffmpeg.input(input)
+        .audio
+        .filter_('atempo', '1.3')
+        .output(output, **options)
+        .run(quiet=True)
+    )
+
+
+def download_episode(playlist_path: Path, episode: Episode) -> t.Tuple[bool, Episode]:
+    try:
+        contents = requests.get(str(episode.url)).content
+        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            input_filename = temp_file.name
+            temp_file.write(contents)
+
+        speedup(input_filename, str((playlist_path / episode.filename).resolve()))
+
+    except Exception:
+        log.exception("Error downloading or speeding up %s", episode.url)
         return False, episode
+
+    finally:
+        try:
+            os.unlink(input_filename)
+        except Exception:
+            pass
+
     return True, episode
 
 
 @click.command()
 @click.option("-i", "--ipod-dir", default="/Volumes/IPOD", help="path to iPod mount point")
 @click.option("-p", "--playlist", default="shuffleupagus", help="name of synced playlist")
-async def sync(ipod_dir, playlist):
-    async with pocket_casts() as api:
-        async with aiohttp.ClientSession() as session:
-            # Get list of episodes in "up next"
-            episodes_from_pocketcasts: t.List[Episode] = await api.up_next()
+def sync(ipod_dir, playlist):
+    with pocket_casts() as api:
+        # Get all podcast titles
+        titles_by_uuid: t.Dict[str, str] = api.podcasts()
 
-            # Get files in the playlist on the iPod
-            ipod_by_uuid: t.Dict[str, Path] = {}
-            playlist_path = Path(ipod_dir) / "iPod_Control" / "Music" / playlist
-            playlist_path.mkdir(parents=True, exist_ok=True)
-            filename_regex = re.compile(r"\d{2}_([a-f0-9-]+)\..+")
-            for file in playlist_path.iterdir():
-                filename_match = filename_regex.match(file.name)
-                if not filename_match:
-                    logging.error("Cannot parse filename %s", file)
-                    continue
-                ipod_by_uuid[filename_match.group(1)] = file
+        # Get list of episodes in "up next"
+        episodes_from_pocketcasts: t.List[Episode] = api.up_next()
 
-            pocketcasts_by_uuid: t.Dict[str, Episode] = {episode.uuid: episode for episode in episodes_from_pocketcasts}
+        # Get files in the playlist on the iPod
+        ipod_by_uuid: t.Dict[str, Path] = {}
+        playlist_path = Path(ipod_dir) / "iPod_Control" / "Music" / playlist
+        playlist_path.mkdir(parents=True, exist_ok=True)
+        filename_regex = re.compile(r"([a-f0-9-]+)\..+")
+        for file in playlist_path.iterdir():
+            filename_match = filename_regex.match(file.name)
+            if not filename_match:
+                log.error("Cannot parse filename %s", file)
+                continue
+            ipod_by_uuid[filename_match.group(1)] = file
 
-            uuids_on_ipod = set(ipod_by_uuid.keys())
-            uuids_from_pocketcasts = set(pocketcasts_by_uuid.keys())
+        pocketcasts_by_uuid: t.Dict[str, Episode] = {episode.uuid: episode for episode in episodes_from_pocketcasts}
 
-            uuids_to_delete = uuids_on_ipod - uuids_from_pocketcasts
-            uuids_to_rename = uuids_on_ipod & uuids_from_pocketcasts
-            uuids_to_download = uuids_from_pocketcasts - uuids_on_ipod
+        uuids_on_ipod = set(ipod_by_uuid.keys())
+        uuids_from_pocketcasts = set(pocketcasts_by_uuid.keys())
 
-            for uuid in uuids_to_delete:
-                ipod_by_uuid[uuid].unlink()
-            for uuid in uuids_to_rename:
-                old_path = ipod_by_uuid[uuid]
-                new_path = old_path.with_name(pocketcasts_by_uuid[uuid].filename)
-                old_path.rename(new_path)
+        uuids_to_delete = uuids_on_ipod - uuids_from_pocketcasts
+        uuids_to_download = uuids_from_pocketcasts - uuids_on_ipod
 
-            for result in asyncio.as_completed([
-                    download_episode(session, playlist_path, pocketcasts_by_uuid[uuid]) for uuid in uuids_to_download
-                    ]):
-                success, episode = await result
+        for uuid in uuids_to_delete:
+            ipod_by_uuid[uuid].unlink()
+
+        with ThreadPool() as pool:
+            for success, episode in pool.imap_unordered(
+                partial(download_episode, playlist_path),
+                (pocketcasts_by_uuid[uuid] for uuid in uuids_to_download),
+            ):
                 if success:
-                    logging.info("Downloaded %s", episode.filename)
+                    log.info("Downloaded %s: %s", episode.title, episode.filename)
+                    try:
+                        tags = EasyID3(playlist_path / episode.filename)
+                        tags["title"] = episode.title
+                        tags["genre"] = "Podcast"
+                        try:
+                            tags["album"] = titles_by_uuid[episode.podcast]
+                        except KeyError:
+                            tags["album"] = "Mystery podcast"
+                        tags.save()
+                    except Exception:
+                        log.exception("Could not rename %r", episode)
+
 
 if __name__ == "__main__":
-    sync(_anyio_backend="asyncio")
+    sync()
